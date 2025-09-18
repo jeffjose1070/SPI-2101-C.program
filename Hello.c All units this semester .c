fad2a687b6be48939e2f6436a2e7a2e37f4c038c
jeffjose1070 <ongangi.21645@students.kyu.ac.ke>
2025-09-18 13:58:45 +0300

    Add files via upload

---
+++ Hello.c All units this semester .c
@@ -0,0 +1,37 @@
+//simple C program (Hello World), single line comment
+/*
+Name:Ongangi Joseph Jeff 
+Reg:CT100/G/21645/2024
+description:
+*/
+
+#include<stdio.h>//pre-processor directive scanf(
+
+//main function 
+int main(){
+    printf("All units this semester:\n");
+    printf("SPH2107-Analague Electronics:\n");
+    printf("SPI2103-Computer Systems And Organisation:\n");
+    printf("UCU2105-Fundamentals Of ICT:\n");
+    printf("SPI2103-Introdiction to computer Applications:\n");
+    printf("SPI2120-Esssenyisls of accounting:\n");
+    printf("UCU2106-communication and information literacy:\n");
+    printf("SPM2104-MaThematics for science:\n");
+    printf("SPI2101-Introduction to programming and algorithms:\n");
+    return 0;
+}    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ helloc.c height, phonenumber, weight .c
@@ -0,0 +1,26 @@
+//simple c program (hello word),single line comment
+/*
+Name: Ongangi Joseph Jeff
+Reg:CT100/G/21645/2024
+description
+*/
+
+#include<stdio.h>//pre-processor scanf
+
+//main function
+int main() {
+    printf("enter your height=");
+    scanf("%d,$height=\n:");
+    printf("your height is%d\n,height=:");
+
+    int phonenumber;
+    printf("enter your phonenumber=");
+    scanf("%d,& your phonenumber=\n");
+    printf("your phonenumber is%d\n,,phonenumber=\n");
+
+    int weight;
+    printf("enter your weightn=:");
+    scanf("%d,& your weight=\n");
+    printf("your weight is %d\n, weight=\n");
+    return 0;
+}
